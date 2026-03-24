7b0f4f912a4e72b0535a5e576d13788e06f34852
kris <krishnakumars0101@gmail.com>
2026-03-24 12:37:27 +0530

    bug:config file bug fixes

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,16 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.externals = ['@prisma/client'];
+    }
+    return config;
+  },
 };
+
 module.exports = {
   allowedDevOrigins: ['172.20.10.5'],
-}
+};
 
 export default nextConfig;
